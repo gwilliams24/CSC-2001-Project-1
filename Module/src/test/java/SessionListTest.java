c7ab7df90f6b2ee351f9b5b831ea18bea07227ec
gwilliams24 <gwilli41@calpoly.edu>
2026-09-18 14:27:08 -0700

    final project!!!

--- Module/src/test/java/SessionListTest.java
+++ Module/src/test/java/SessionListTest.java
@@ -40,10 +40,23 @@ class SessionListTest {
 
 
     //Tests for display method:
+     @Test
+     void display1() {
+         assertEquals("106 Econ Tinsley 2026-09-09 Baker 2\n--------------------\n",
+                 SessionList.display(new SessionList(b, null)));
+     }
 
-   // @Test
- //   void display_1(){
-   //     assertEquals("106 Econ Tinsley 2026-09-09 Baker 2\n--------------------\n", SessionList.display(new SessionList(b, null)));
+     @Test
+     void display2(){
+        assertEquals("106 Econ Tinsley 2026-09-09 Baker 2\n--------------------\n" +
+                "107 Math Tinsley 2026-01-29 Frost 3\n--------------------\n",
+                SessionList.display(new SessionList(b, new SessionList(c, null))));
+     }
+
+     @Test
+     void display3(){
+        assertEquals("", SessionList.display(null));
+     }
 
 
     //Tests for searchBy (both ID and mentor)
@@ -77,6 +90,27 @@ class SessionListTest {
         assertEquals(null, SessionList.searchByMentor(null, "Gin"));
     }
 
+    // Tests for remove session method:
+    @Test
+    void removeSession1(){
+        assertEquals(new SessionList(new Session(101, "Business", "Arden", "2026-09-01", "03-302", 0, 5),
+                new SessionList(new Session(102, "Math", "Wetzel", "2026-09-04", "38-220", 0, 12),
+                        new SessionList(new Session(104, "Statistics", "Chance", "2026-09-10", "25-205", 0, 3),
+                                new SessionList(new Session(105, "Journalism", "Riley", "2026-09-11", "26-210", 0, 1), null)))),
+                SessionList.removeSession(a, 103));
+    }
+
+    @Test
+    void removeSession2(){
+        assertEquals(a, SessionList.removeSession(a, 107));
+    }
+
+    @Test
+    void removeSession3(){
+        assertEquals(null, SessionList.removeSession(null, 101));
+    }
+
+
     // Tests for replaceSession method
     @Test
     void replaceSession1() {
@@ -99,6 +133,30 @@ class SessionListTest {
         assertEquals(null, SessionList.replaceSession(null, c));
     }
 
+    // Tests for registerParticipant method
+    @Test
+    void registerParticipant1(){
+        assertEquals(new SessionList(new Session(106, "Econ", "Tinsley", "2026-09-09", "Baker", 1, 2), null),
+                (SessionList.registerParticipant(new SessionList(b, null), 106)));
+    }
+
+    @Test
+    void registerParticipant2(){
+        assertThrows(IllegalArgumentException.class,
+                () -> SessionList.registerParticipant(new SessionList(new Session(106, "Econ", "Tinsley", "2026-09-09", "Baker", 2, 2), null), 106));
+    }
+
+    @Test
+    void registerParticipant3(){
+        assertThrows(IllegalArgumentException.class,
+                () -> SessionList.registerParticipant(new SessionList(new Session(106, "Econ", "Tinsley", "2026-09-09", "Baker", 2, 2), null), 107));
+    }
+
+    @Test
+    void registerParticipant4(){
+        assertEquals(new SessionList(new Session(106, "Econ", "Tinsley", "2026-09-09", "Baker", 1, 2), a),
+                (SessionList.registerParticipant(new SessionList(b, a), 106)));
+    }
 
 }
 
